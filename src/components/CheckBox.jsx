f79e947e3b15d5b192f5dc8de6c5f257b80fd06b
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-08-28 21:45:00 +0100

    first-commit

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -4,14 +4,14 @@ const CheckBox = ({ title, checked, onChange }) => {
   return (
     <div className="relative">
       <div className="flex justify-between items-center">
-        <label className="text-[#1F2128] font-montserrat">{title}</label>
+        <label className="text-[#1F2128] font-montserrat font-[400] text-[16px]">{title}</label>
         <div className="relative">
           <input
             type="checkbox"
             checked={checked}
             onChange={onChange}
             className={`
-              appearance-none border-2 border-[#BDBDBD] h-6 w-6 rounded-[5px]
+              appearance-none border-2 border-[#BDBDBD] h-[23px] w-[23px] rounded-[6px]
               checked:bg-[#2469F6] checked:border-none
               focus:outline-none
               transition-all duration-200 ease-in-out
@@ -20,7 +20,7 @@ const CheckBox = ({ title, checked, onChange }) => {
           />
           {checked && (
             <svg
-              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white"
+              className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white"
               width="12"
               height="9"
               viewBox="0 0 12 9"
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -41,8 +41,8 @@ const MainPage = () => {
 
   return (
     <div className='flex flex-col items-center'>
-      <div className='shadow-[0_8px_15px_0px_rgba(20,20,20,0.12),0_0_4px_0px_rgba(20,20,20,0.1)] rounded-[8px] bg-[#fff] md:w-[400px] w-[300px] mt-24 p-6'>
-        <div className='border-b-2 border-[#CDCDCD] pb-4'>
+      <div className='shadow-[0_8px_15px_0px_rgba(20,20,20,0.12),0_0_4px_0px_rgba(20,20,20,0.1)] rounded-[8px] bg-[#fff] md:w-[370px] w-[300px]  mt-24 p-6'>
+        <div className='border-b-[0.7px] border-[#CDCDCD] pb-4'>
           <CheckBox 
             title='All Pages'
             checked={checkboxes.allPages}
@@ -50,7 +50,7 @@ const MainPage = () => {
           />
         </div>
 
-        <div className='flex flex-col gap-6 py-4 border-b-2 border-[#CDCDCD]'>
+        <div className='flex flex-col gap-6 py-4 border-b-[0.7px] border-[#CDCDCD]'>
           {['page1', 'page2', 'page3', 'page4'].map((page, index) => (
             <CheckBox
               key={page}
