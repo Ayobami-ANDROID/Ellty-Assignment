059128c667d46de0b0655e6362333cce1d60a350
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-08-29 13:28:02 +0100

    animate checkbok

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,45 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CheckBox = ({ title, checked, onChange }) => {
-  return (
-    <div className="relative">
-      <div className="flex justify-between items-center">
-        <label className="text-[#1F2128] font-montserrat font-[400] text-[14px] font-text">{title}</label>
+    const [isHovered, setIsHovered] = useState(false);
+    const [isPressed, setIsPressed] = useState(false);
+
+    return (
         <div className="relative">
-          <input
-            type="checkbox"
-            checked={checked}
-            onChange={onChange}
-            className={`
-              appearance-none border-2 border-[#BDBDBD] h-[23px] w-[23px] rounded-[6px]
-              checked:bg-[#2469F6] checked:border-none
-              focus:outline-none
+            <div className="flex justify-between items-center">
+                <label className="text-[#1F2128] font-montserrat font-[400] text-[14px] font-text">{title}</label>
+                <div className="relative">
+                    <input
+                        type="checkbox"
+                        checked={checked}
+                        onChange={onChange}
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
+                        onMouseDown={() => setIsPressed(true)}
+                        onMouseUp={() => setIsPressed(false)}
+                        className={`
+              appearance-none border-2 h-[23px] w-[23px] rounded-[6px]
               transition-all duration-200 ease-in-out
-              ${checked ? 'hover:ring-2 hover:ring-blue-200' : ''}
+              ${checked
+                                ? 'bg-[#2469F6] border-0 hover:bg-[#5087F8]'
+                                : 'border-[#CDCDCD] hover:border-[#BDBDBD] hover:bg-white'
+                            }
+              ${isPressed && checked ? 'ring-2 ring-blue-200 bg-[#2469F6]' : ''}
+              ${isPressed && !checked ? 'bg-white border-[#BDBDBD]' : ''}
+              focus:outline-none
             `}
-          />
-          {checked && (
-            <svg
-              className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white"
-              width="12"
-              height="9"
-              viewBox="0 0 12 9"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M1 5L3.82353 8L11 1"
-                stroke="white"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          )}
+                    />
+                    {checked && (
+
+                        <svg width="19" height="14" viewBox="0 0 19 14" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white pointer-events-none">
+                            <path d="M1 7.6L7.03451 12.9672C7.05497 12.9854 7.08626 12.9837 7.1047 12.9635L18 1" stroke="white" stroke-linecap="round" />
+                        </svg>
+                    )}
+                    {!checked && isHovered && (
+                        <svg
+                            className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none"
+                            width="19"
+                            height="14"
+                            viewBox="0 0 19 14"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M1 7.6L7.03451 12.9672C7.05497 12.9854 7.08626 12.9837 7.1047 12.9635L18 1"
+                                stroke={isPressed ? "#878787" : "#E3E3E3"}
+                                strokeLinecap="round"
+                            />
+                        </svg>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CheckBox;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { toast} from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import CheckBox from '../components/CheckBox';
 import Button from '../components/Button';
 
@@ -41,23 +42,34 @@ const MainPage = () => {
   };
 
   const handleButtonClick = () => {
-    const selectedPages = Object.entries(checkboxes)
-      .filter(([key, value]) => key !== 'allPages' && value)
-      .map(([key]) => key.charAt(0).toUpperCase() + key.slice(1))
-      .join(', ');
-
+    let selectedPages = [];
     if (checkboxes.allPages) {
-      toast.success('All Pages clicked!');
-    } else if (selectedPages) {
-      toast.success(`Selected pages: ${selectedPages}`);
+      selectedPages = ['All Pages'];
     } else {
-      toast.info('No pages selected');
+      for (let i = 1; i <= 4; i++) {
+        if (checkboxes[`page${i}`]) {
+          selectedPages.push(`Page ${i}`);
+        }
+      }
     }
+
+    const message = selectedPages.length > 0
+      ? `Selected pages: ${selectedPages.join(', ')}`
+      : 'No page selected';
+
+    toast(message, {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
   };
 
   return (
     <div className='flex flex-col items-center'>
-      <div className='shadow-[0_8px_15px_0px_rgba(20,20,20,0.12),0_0_4px_0px_rgba(20,20,20,0.1)] rounded-[6px] border-[1px] border-[#EEEEEE] bg-[#fff] md:w-[370px] w-[300px] mt-[85px] p-6'>
+      <div className='shadow-[0_8px_15px_0px_rgba(20,20,20,0.12),0_0_4px_0px_rgba(20,20,20,0.1)] rounded-[8px] bg-[#fff] md:w-[370px] w-[300px] mt-24 p-6'>
         <div className='border-b-[0.7px] border-[#CDCDCD] pb-4'>
           <CheckBox 
             title='All Pages'
@@ -81,6 +93,7 @@ const MainPage = () => {
           <Button onClick={handleButtonClick} />
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
