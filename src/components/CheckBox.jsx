9e86b35f9cdd48a4f490d64a378312dab665d9a7
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-08-30 13:11:01 +0100

    border

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -18,21 +18,25 @@ const CheckBox = ({ title, checked, onChange }) => {
                         onMouseDown={() => setIsPressed(true)}
                         onMouseUp={() => setIsPressed(false)}
                         className={`
-              appearance-none border-2 h-[23px] w-[23px] rounded-[6px]
-              transition-all duration-200 ease-in-out
-              ${checked
-                                ? 'bg-[#2469F6] border-0 hover:bg-[#5087F8] h-[23px] w-[23px]'
-                                : 'border-[#CDCDCD] hover:border-[#BDBDBD] hover:bg-white h-[23px] w-[23px]'
+                            appearance-none border-2 h-[23px] w-[23px] rounded-[6px]
+                            transition-all duration-200 ease-in-out
+                            ${checked
+                                ? 'bg-[#2469F6] border-[#2469F6] hover:bg-[#5087F8] hover:border-[#5087F8]'
+                                : 'border-[#CDCDCD] hover:border-[#BDBDBD] hover:bg-white'
                             }
-              ${isPressed && checked ? 'ring-2 ring-blue-200 bg-[#2469F6] h-[23px] w-[23px] border-0' : ''}
-              ${isPressed && !checked ? 'bg-white border-[#BDBDBD] h-[23px] w-[23px]' : ''}
-              focus:outline-none
-            `}
+                            ${isPressed && checked ? 'ring-2 ring-blue-200 bg-[#2469F6] border-[#2469F6]' : ''}
+                            ${isPressed && !checked ? 'bg-white border-[#BDBDBD]' : ''}
+                            focus:outline-none
+                        `}
+                        style={{
+                            boxSizing: 'border-box',
+                            minWidth: '23px',
+                            minHeight: '23px'
+                        }}
                     />
                     {checked && (
-
                         <svg width="19" height="14" viewBox="0 0 19 14" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white pointer-events-none">
-                            <path d="M1 7.6L7.03451 12.9672C7.05497 12.9854 7.08626 12.9837 7.1047 12.9635L18 1" stroke="white" stroke-linecap="round" />
+                            <path d="M1 7.6L7.03451 12.9672C7.05497 12.9854 7.08626 12.9837 7.1047 12.9635L18 1" stroke="white" strokeLinecap="round" />
                         </svg>
                     )}
                     {!checked && isHovered && (
